0f1422af8a12d1315571f0f37cd29bd24b7552d4
Preston <prestonosoro56@gmail.com>
2022-12-15 21:53:55 +0300

    atomic commit

---
+++ src/script.js
@@ -0,0 +1,39 @@
+ 
+ async function generateJoke() {
+  let url;
+  let category =  document.getElementById("select-list").value;
+
+  if(category !== "select category") {
+    url ="https://api.chucknorris.io/jokes/random?category=" + category;
+  } else {
+       url ="https://api.chucknorris.io/jokes/random";
+  }
+  await fetch(url)
+    .then((res) => res.json())
+    //get raw json data
+    .then((obj) => {
+      this.obj = obj;
+      console.log(this.obj.value);
+      // display the joke when the webpage loads
+      //document.getElementById("image").src = this.obj.icon_url;
+      document.getElementById("joke").innerHTML = this.obj.value;
+    })
+    .catch(function () {
+      console.log("error");
+    });
+}
+
+window.onload = async function (e) {
+  generateJoke();
+  document.getElementById("generate").addEventListener('click', generateJoke);
+  e.preventDefault()
+};
+
+async function readJson() {
+  let res = await fetch("../db.json");
+  let json = await res.json();
+  console.log(json);
+}
+
+
+console.log(`Hello from JavaScript!`);
