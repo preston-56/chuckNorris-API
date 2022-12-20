5c16913e60c2e928b448a10b48dcb80fb4ff72a2
Preston <prestonosoro56@gmail.com>
2022-12-21 01:49:40 +0300

    add pseudo.js

--- pseudo.js
+++ pseudo.js
@@ -1,10 +1,3 @@
-
-//setup your event handlers here
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-})
 async function getJoke() {
   let url = 'http://localhost:3000/jokes' 
       await fetch(url)
@@ -16,31 +9,40 @@ async function getJoke() {
     <div class="card p-2 mt-2 w-75" id="new-joke">
     
     <h4 id="content">
-    <p><button class="btn btn-primary btn-sm btn-lg float-end" id="delete-btn">Delete</button></p>
     <i class="fas fa-quote-left"></i>
     <p id="random-joke">${item.value} </p>
     <i class="fas fa-quote-right"></i>
-    
-    
     <h4>
-    
     </div>
-   
     `;
           }
         });
-
+/*<p><button class="btn btn-primary btn-sm btn-lg float-end" id="delete-btn">Delete</button></p>*/
   }
 
 getJoke()
 
-  // async function createJoke(item) {}
+// async function createJoke(item) {
+  
+// }
+
+
+
+//setup your event handlers here
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+})
+
+  
+
 
 
 const deleteButton = document.getElementById('delete-btn');
   deleteButton.addEventListener("click", function(event) {
     if (event.target.id === 'delete-btn') {
-    fetch(`http://localhost:3000/categories/dev`, {
+    fetch(`http://localhost:3000/jokes`, {
     method: "DELETE",
     headers: {
     "content-type": "application/json",
@@ -48,10 +50,9 @@ const deleteButton = document.getElementById('delete-btn');
     }
   }).then(resp => resp.json())
   .then(() => {
-  // form.innerHTML = "";
-  // form.remove();
   document.getElementById('content').innerHTML = "";
   deleteButton.style.display = "none";
   })
 }
 })
+
